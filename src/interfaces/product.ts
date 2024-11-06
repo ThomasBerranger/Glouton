5ae3dc5b28b9c977c3efbc952785e0bbba6a1208
Thomas Berranger <tberranger@hotmail.fr>
2024-11-06 18:21:14 +0100

    home products list and recipes

--- src/interfaces/product.ts
+++ src/interfaces/product.ts
@@ -3,7 +3,7 @@ import type { ExpirationDate } from '@/interfaces/expiration-date'
 export interface Product {
     name: string;
     description: string | null;
-    image: string | null;
+    image: string | undefined;
     finishedAt: string | null;
     addedToListAt: string | null;
     expirationDates: ExpirationDate[];
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,46 +1,37 @@
 <script lang="ts" setup>
 import { onMounted, ref, watch } from "vue";
 import "vue3-carousel/dist/carousel.css";
-import store from "@/assets/store";
+import store from "@/assets/store"; // todo: remplacer par pinia
 import axios from "axios";
 import type { Product } from "@/interfaces/product";
+import type { Recipe } from "@/interfaces/recipe";
 
 const products = ref<Record<number, Product>>({});
+const recipes = ref<Record<number, Recipe>>({});
 
 onMounted(async () => {
-  console.log(store.state.token);
+  try {
+    const [productsResponse, recipesResponse] = await Promise.all([
+      axios.get("https://glouton-fd999217b246.herokuapp.com/products?limit=8", {
+        headers: { Authorization: `Bearer ${store.state.token}` },
+      }),
+      axios.get("https://glouton-fd999217b246.herokuapp.com/recipes", {
+        headers: { Authorization: `Bearer ${store.state.token}` },
+      }),
+    ]);
 
-  axios
-    .get("https://glouton-fd999217b246.herokuapp.com/products?limit=7", {
-      headers: { Authorization: `Bearer ${store.state.token}` },
-    })
-    .then((response) => {
-      products.value = response.data;
-      products.value.forEach((element) => {
-        console.log(element.name);
-        element.expirationDates.forEach((element) => {
-          console.log(element.date);
-        });
-      });
-    })
-    .catch((error) => {
-      console.error("Error during poducts fetch:", error);
-    });
+    products.value = productsResponse.data;
+    recipes.value = recipesResponse.data;
+  } catch (error) {
+    console.error("Error during fetch:", error);
+  }
 });
 </script>
 
 <template>
   <div class="screen-height">
-
-
-    <div class="h-2/5 w-screen grid grid-cols-4 gap-2 border border-1 border-blue-600">
-
-
-      <div
-        v-for="product in products"
-        :key="product.id"
-        class="flex items-center relative border border-1 border-gray-200 h-1/2"
-      >
+    <section div class="h-3/5 w-screen p-1 grid gap-1 grid-cols-4 grid-rows-3">
+      <div v-for="product in products" class="relative">
         <img
           :src="product.image"
           :alt="product.name"
@@ -48,24 +39,21 @@ onMounted(async () => {
         />
         <div class="absolute bottom-0 right-0 p-2 bg-red-300">3</div>
       </div>
-
-      
+      <div class="rounded-xl bg-green-200"></div>
+      <div class="rounded-xl bg-green-200"></div>
       <div
-        v-for="product in products"
-        :key="product.id"
-        class="flex items-center relative border border-1 border-gray-200 h-1/2"
+        class="col-span-2 flex justify-center items-center relative rounded-xl border border-1 border-gray-200 text-white font-semibold bg-slate-400"
       >
-        <img
-          :src="product.image"
-          :alt="product.name"
-          class="w-full h-full object-contain"
-        />
-        <div class="absolute bottom-0 right-0 p-2 bg-red-300">3</div>
+        Tout voir
       </div>
-
-
+    </section>
+    <div class="h-1/5 border border-1 border-red-600">
+      <ul>
+        <li v-for="recipe in recipes" class="p-2">
+          - {{ recipe.name }} : {{ recipe.duration }}
+        </li>
+      </ul>
     </div>
-    <div class="h-2/5 border border-1 border-red-600">Recettes</div>
-    <div class="h-1/5 border border-1 border-green-600">Shopping List</div>
+    <div class="h-1/5 border border-1 border-green-600 p-2">Shopping List</div>
   </div>
 </template>
